20f51638bdcb9f8e79ea6bf9dbce985a666e769e
Rodrigo Caus <caus@Bill.localdomain>
2018-05-21 01:30:46 +0000

    Plot está funcional. Falta fixar valores do eixo y, colocar labels nos eixos, e determinar a frequencia do sinal em bpm (associar o sinal ao tempo real)

--- src/plot_oximeter_serial.m
+++ src/plot_oximeter_serial.m
@@ -2,8 +2,9 @@ pkg load instrument-control
 
 # Variaveis e constantes de uso
 SAMPLES_TIMEOUT = 1000;
-BYTES_READ = 4;
-SERIAL_PORT = "com3";
+	# Formato de leitura: x.xx\r\n
+BYTES_READ = 6;
+SERIAL_PORT = "com5";
 
 # Inicializa a porta serial
 if(exist('serial') != 3)
@@ -14,17 +15,17 @@ endif
 s1 = serial(SERIAL_PORT, 19200);
 srl_flush(s1);
 
-# Gerenciar o Plot
-
-
 # Leitura continua até apertar a letra 'q'
 press = "0";
-y_read = ones(1, SAMPLES_TIMEOUT);
+y_read = double(ones(1, SAMPLES_TIMEOUT));
 count = 1;
+
+plotHandle = plot(y_read);
+
 while ~isequal(press, "q")
 
-	y_read(count) = double(srl_read(s1, BYTES_READ));
-
+	y_read(count) = str2num(char(srl_read(s1, BYTES_READ)));
+	set(plotHandle,'YData', y_read);
 	count = count+1;
 	if count >= SAMPLES_TIMEOUT
 		count = 1;
