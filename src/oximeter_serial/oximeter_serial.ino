2619bd8861896a4b21e66d6cb8d882a33d948ce9
Rodrigo Caus <caus@Bill.localdomain>
2018-05-21 01:28:49 +0000

    Programa envia um double pela serial a cada ~10ms. Faltam testes com o sensor

--- src/oximeter_serial/oximeter_serial.ino
+++ src/oximeter_serial/oximeter_serial.ino
@@ -6,26 +6,27 @@
  */
 
 
-#define PI 3.14159
+
 #define OXI_PIN A0
 
 double i;
-double oximeter_voltage;
+double oximeter;
 unsigned long sys_time;
 
 
 void setup() {
   Serial.begin(19200);
   delay(3000);
-  oximeter_voltage = 0.0;
-  i = 0.0;
+  oximeter = 0;
+  i = 0;
 }
 
 void loop() {
  //oximeter_voltage = 5.0*(analogRead(OXI_PIN)/1024.0);
- oximeter_voltage = cos(i)+1;
- Serial.write((byte *) &oximeter_voltage, sizeof(oximeter_voltage));
- i += 0.01;
+ oximeter = cos(i)+1;
+ Serial.println(oximeter);
+ i += 0.1;
  if(i > 2*PI)
-    i = 0.0;
+    i = 0;
+ delay(10);
 }
