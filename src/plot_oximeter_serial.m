2ffb270b5b25e503b83cd04f0822306f58553f3a
Rodrigo Caus <caus@Bill.localdomain>
2018-05-27 18:06:24 +0000

    Adicionados os 'labels' de x e y, com y imprimindo tensao fixa de 0 a 5v e x imprimindo intervalos de 10s. Falta calcular frequencia e imprimir em uma caixa de texto no grafico

--- src/plot_oximeter_serial.m
+++ src/plot_oximeter_serial.m
@@ -1,10 +1,13 @@
+# Necessita do pacote instrument-control (http://wiki.octave.org/Instrument_control_package)
 pkg load instrument-control
 
-# Variaveis e constantes de uso
+# Número de amostras lidas por ciclo
 SAMPLES_TIMEOUT = 1000;
-	# Formato de leitura: x.xx\r\n
+# Formato de leitura: x.xx\r\n
 BYTES_READ = 6;
+# Porta serial usada pode variar
 SERIAL_PORT = "com5";
+BAUD_RATE = 19200;
 
 # Inicializa a porta serial
 if(exist('serial') != 3)
@@ -12,25 +15,42 @@ if(exist('serial') != 3)
 	pause(3);
 	exit;
 endif
-s1 = serial(SERIAL_PORT, 19200);
+s1 = serial(SERIAL_PORT, BAUD_RATE);
 srl_flush(s1);
 
-# Leitura continua até apertar a letra 'q'
-press = "0";
+
 y_read = double(ones(1, SAMPLES_TIMEOUT));
 count = 1;
 
+# Gerenciamento de plot
 plotHandle = plot(y_read);
+grid("on");
+ylabel("Tensao (V)");
+xlabel("Tempo (s)");
+	# Tensão limitada de 0 a 5V
+ylim([0.0 5.0]);
+	# Limita o eixo x a SAMPLES_TIMEOUT/10 amostras por segundo
+	# Deve estar sincronizado com o Arduino
+xticks([0:SAMPLES_TIMEOUT/10:SAMPLES_TIMEOUT]);
+xticklabels(num2cell(0:10));
 
+
+# Leitura continua até apertar a letra 'q'
+press = "0";
 while ~isequal(press, "q")
 
 	y_read(count) = str2num(char(srl_read(s1, BYTES_READ)));
 	set(plotHandle,'YData', y_read);
 	count = count+1;
+
+	# Volta para o inicio
 	if count >= SAMPLES_TIMEOUT
 		count = 1;
 	endif
+
+	# Verifica se houve tecla pressionada
 	press = kbhit(1);
+
 endwhile
 
 srl_close(s1);
