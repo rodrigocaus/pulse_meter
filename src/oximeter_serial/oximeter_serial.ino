02bc4217b2601d862ae455741a83aaa4a50edc37
Rodrigo Caus <caus@Bill.localdomain>
2018-05-28 03:38:44 +0000

    Alterada constante de incremento no teste (usando cosseno) para deixar mais próximo do batimento humano em BPM

--- src/oximeter_serial/oximeter_serial.ino
+++ src/oximeter_serial/oximeter_serial.ino
@@ -23,7 +23,7 @@ void loop() {
  //oximeter = 5.0*(analogRead(OXI_PIN)/1024.0);
  oximeter = max(2*cos(i), 0.5);
  Serial.println(oximeter);
- i += 0.1;
+ i += 0.08;
  if(i > 2*PI)
     i = 0;
  delay(10);
